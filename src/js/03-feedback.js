f2e33891f386a5d73c8cfc1e0fd7336623dfde5b
ADudys <ania.dudys99@gmail.com>
2023-07-16 14:27:36 +0200

    Task 3 was done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const { email, message } = form.elements;
+
+form.addEventListener('input', throttle(getInputData, 500));
+form.addEventListener('submit', submit);
+
+function getInputData(event) {
+  const inputData = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+}
+
+function submit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(email);
+  localStorage.removeItem(message);
+}
