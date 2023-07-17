ca844ca8b436b105a46e97034d601aa307cb4958
ADudys <ania.dudys99@gmail.com>
2023-07-17 16:50:53 +0200

    Inputs are automaticly filled

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,23 @@
 import throttle from 'lodash.throttle';
 
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 const form = document.querySelector('.feedback-form');
 const { email, message } = form.elements;
+
+const LOCAL_KEY = 'feedback-form-state';
+
+if (localStorage.length !== 0) {
+  input.value = JSON.parse(localStorage.getItem(LOCAL_KEY)).email;
+  textarea.value = JSON.parse(localStorage.getItem(LOCAL_KEY)).message;
+}
 
 form.addEventListener('input', throttle(getInputData, 500));
 form.addEventListener('submit', submit);
 
 function getInputData(event) {
   const inputData = { email: email.value, message: message.value };
-  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(inputData));
 }
 
 function submit(event) {
